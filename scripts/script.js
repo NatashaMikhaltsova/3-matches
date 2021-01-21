1319ea41ce8ae06794f23f98b646aaf0b7a4a948
NatashaMikhaltsova <natashamikhaltsova@gmail.com>
2021-01-22 01:14:14 +0300

    joing checkRow and checkColumn to checkMatches

--- scripts/script.js
+++ scripts/script.js
@@ -250,14 +250,89 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   } */
 
+  //update score
+  let updateScore = (matches) => {
+    for(let i = 0; i < matches.length; i++) {
+      let match = matches[i];
+      score += match.length;
+      scoreDisplay.innerHTML = score;
+    }
+    
+  }
+  //remove matches
+  let removeMatches = (matches) => {
+    for(let i = 0; i < matches.length; i++) {
+      let match = matches[i];
 
+      for (let j = 0; j < match.length; j++) {
+        if(match.horizontal) {
+          squares[match.element + j].style.backgroundColor = 'white';
+        } else {
+          squares[match.element + j * width].style.backgroundColor = 'white';
+        }
+      }
+    }
+  }
+  //check for matches
+  let checkMatches = () => {
+    let matches = [];
+
+    for (let i = 0; i < 62; i++) {
+
+      let rowIndex = Math.trunc(i/width);
+      let colCounter = 1;
+      let rowCounter = 1;
+      
+      //search for matches in row
+      while ( Math.trunc((i + rowCounter) / width) === rowIndex && 
+      squares[i].style.backgroundColor === squares[i + rowCounter].style.backgroundColor) {
+        rowCounter++;
+      } 
+
+      if (rowCounter > 2) {
+        matches.push({ element: i,
+          length: rowCounter,
+          horizontal: true
+        })
+      }
+
+      
+      //search for matches in column
+      while ( (i + width * colCounter) < 64 && 
+      squares[i].style.backgroundColor === squares[i + width * colCounter].style.backgroundColor) {
+        colCounter++;
+      }
+
+      if (colCounter > 2) {
+        matches.push({ element: i,
+          length: colCounter,
+          horizontal: false
+        })
+      }
+    }
+    console.log(matches)
+    
+    removeMatches(matches);
+    updateScore(matches);
+    
+  }
+
+  
+
+  
+
+
+
+  //Determine if a move exists in a Bejeweled/match 3 game
   /* //checkRowForFour();
   checkColumnForFour();
   //checkRowForThree();
   checkColumnForThree(); */
   moveDown();
-  checkRow();
-  checkColumn();
+ /*  checkRow();
+  checkColumn(); */
+  checkMatches();
+  
   
 
 
@@ -267,8 +342,10 @@ document.addEventListener('DOMContentLoaded', () => {
     checkColumnForFour();
     //checkRowForThree();
     checkColumnForThree(); */
-    checkRow();
-    checkColumn();
+    /* checkRow();
+    checkColumn(); */
+    checkMatches();
+    
     
   }, 1000);
 
